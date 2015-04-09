48c6e1ef890e6da73a52edcca8c455b3f991f440
David Patnode <draknor@gmail.com>
2015-04-09 11:59:57 -0500

    allow search term

--- lib/highrise/searchable.rb
+++ lib/highrise/searchable.rb
@@ -9,7 +9,7 @@ module Highrise
       # Ex: Highrise::Person.search(:email => "john.doe@example.com", :country => "CA")
       # Available criteria are: city, state, country, zip, phone, email
       def search(options = {})
-        search_params = options.inject({}) { |h, (k, v)| h["criteria[#{k}]"] = v; h }
+        search_params = options.inject({}) { |h, (k,v)|  h[(k == :term ? k : "criteria[#{k}]")] = v; h}
         # This might have to be changed in the future if other non-pagable resources become searchable
         if self.respond_to?(:find_all_across_pages)
           self.find_all_across_pages(:from => "/#{self.collection_name}/search.xml", :params => search_params)
